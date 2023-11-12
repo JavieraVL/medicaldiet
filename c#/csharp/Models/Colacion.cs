cb847a5a48d6b497cfb02381a2e7498d00719652
JavieraVL <78371852+JavieraVL@users.noreply.github.com>
2023-11-12 17:40:14 -0300

    Add files via upload

---
+++ c#/csharp/Models/Colacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp.Models
+{
+    public partial class Colacion
+    {
+        public Colacion()
+        {
+            IngredienteDieta = new HashSet<IngredienteDietum>();
+        }
+
+        public int Id { get; set; }
+        public int DietaId { get; set; }
+        public string Tipo { get; set; } = null!;
+        public string Horario { get; set; } = null!;
+
+        public virtual DietaOriginal Dieta { get; set; } = null!;
+        public virtual ICollection<IngredienteDietum> IngredienteDieta { get; set; }
+    }
+}
---
+++ c#/csharp/Models/DietaOriginal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp.Models
+{
+    public partial class DietaOriginal
+    {
+        public DietaOriginal()
+        {
+            Colacions = new HashSet<Colacion>();
+        }
+
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Categoria { get; set; }
+        public DateTime? Fecha { get; set; }
+        public string? Pacienterut { get; set; }
+
+        public virtual Paciente? PacienterutNavigation { get; set; }
+        public virtual ICollection<Colacion> Colacions { get; set; }
+    }
+}
